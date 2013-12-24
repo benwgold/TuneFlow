f961dc0899e919726e681d11aea249b79a306503
Ben Goldberger <bengold@Bens-MacBook-Pro.local>
2013-12-24 15:39:50 -0600

    Initial Commit

---
+++ TuneFlow/TFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TFAppDelegate.h
+//  TuneFlow
+//
+//  Created by Ben Goldberger on 12/24/13.
+//  Copyright (c) 2013 Ben Goldberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TuneFlow/TFViewController.m
@@ -0,0 +1,29 @@
+//
+//  TFViewController.m
+//  TuneFlow
+//
+//  Created by Ben Goldberger on 12/24/13.
+//  Copyright (c) 2013 Ben Goldberger. All rights reserved.
+//
+
+#import "TFViewController.h"
+
+@interface TFViewController ()
+
+@end
+
+@implementation TFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
